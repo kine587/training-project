81a7e94220d020828b197710e80861eb2eb0b03a
KineBru <kine.kodehode@gmail.com>
2025-12-18 12:23:48 +0100

    added traininglist path

--- training-project/src/Routes/AppRoutes.jsx
+++ training-project/src/Routes/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "../Pages/Home";
+import Home from "../Pages/Home.jsx";
+import TrainingList from "../Pages/TrainingList.jsx";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/training" element={<TrainingList />} />
     </Routes>
   );
 }
