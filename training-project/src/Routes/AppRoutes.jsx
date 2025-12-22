eaad0c9a21d0fbf0f77ec30520b421ea8f45fd3d
KineBru <kine.kodehode@gmail.com>
2025-12-22 14:38:49 +0100

    added logg toroutes

--- training-project/src/Routes/AppRoutes.jsx
+++ training-project/src/Routes/AppRoutes.jsx
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "../Pages/Home.jsx";
 import TrainingList from "../Pages/TrainingList.jsx";
+import TrainingDetail from "../Pages/TrainingDetail.jsx";
+import TrainingLogg from "../Pages/TrainingLogg.jsx";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/training" element={<TrainingList />} />
+      <Route path="/training/:Id" element={<TrainingDetail />} />
+      <Route path="/Logg" element={<TrainingLogg />} />
     </Routes>
   );
 }
